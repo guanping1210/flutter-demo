25e2a7cae061ca7731b005ee29838b455a757751
官萍 <ping.guan@cyber-insight.com>
2021-08-13 13:50:14 +0800

    test

--- flutter_demo_01/lib/main.dart
+++ flutter_demo_01/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(new MyApp());
@@ -7,8 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // final wordPair = new WordPair().random();
+
     return new MaterialApp(
-      title: 'flutter_demo_01',
-    );
+        title: 'flutter_demo_01',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('cesi'),
+            ),
+            body: Column(children: <Widget>[
+              Text('23333'),
+              Center(child: Image.asset('lib/images/th.jpg'))
+            ])));
   }
 }
--- flutter_demo_01_mac/lib/main.dart
+++ flutter_demo_01_mac/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 /**
  * 计数器demo
  */
@@ -20,22 +22,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
-      title: 'count demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // home: new MyHomePage(title: 'home page'),
-      // 注册命名路由，如果用了注册路由，那么home就不能直接指定了
-      // 需要通过 / 来注册首页路由
-      routes: {
-        'new_page': (context) => NewRoute(),
-        "demo1": (context) => Demo1(),
-        "demo2": (context) => Demo2(),
-        "demo3": (context) => Demo3(),
-        // 注册首页路由
-        "/": (context) => new MyHomePage(title: 'new route home page'),
-      }
-    );
+        title: 'count demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: new MyHomePage(title: 'home page'),
+        // 注册命名路由，如果用了注册路由，那么home就不能直接指定了
+        // 需要通过 / 来注册首页路由
+        routes: {
+          'new_page': (context) => NewRoute(),
+          "demo1": (context) => Demo1(),
+          "demo2": (context) => Demo2(),
+          "demo3": (context) => Demo3(),
+          // 注册首页路由
+          "/": (context) => new MyHomePage(title: 'new route home page'),
+        },
+        // 该方法可以检测路由，在新路由打开之前可以执行一系列操作，例如用户菜单权限控制
+        onGenerateRoute: (RouteSettings settings) {
+          return MaterialPageRoute(build: (conext) {
+            String routeName = setting.name;
+            // ...
+          });
+        });
   }
 }
 
@@ -44,7 +52,7 @@ class MyApp extends StatelessWidget {
 //    StatefulWidget（有状态的组件）类, 本身是不变的 + State（状态）类，在widget生命周期中可能会发生变化
 // StatelessWidget，也是有状态组件，但是状态是不可更改的
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}): super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   // _MyHomePageState类是MyHomePage类对应的状态类
@@ -73,7 +81,6 @@ class _MyHomePageState extends State<MyHomePage> {
   // 第一次创建是，fultter框架就会调用Widget中的build方法来构建widget树，然后渲染到屏幕上
   @override
   Widget build(BuildContext context) {
-
     // Scaffold是页面脚手架，提供来导航栏、标题、主屏幕Widget树的body属性
     return new Scaffold(
       appBar: new AppBar(
@@ -81,26 +88,31 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: new Center(
         child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text('一共点击的次数：'),
-            new Text('$_counter', style: Theme.of(context).textTheme.headline4),
-            // 添加一个新的按钮，点击可以跳转去新的页面
-            FlatButton(
-              child: Text('打开新页面'),
-              textColor: Colors.blue,
-              // 按钮点击事件
-              onPressed: () {
-                // Navigator 是一个路由管理的组件，提供了打开和退出路由的方法，通过栈来保存路由历史
-                //   .push 打开新页面  .pop 回到上一个页面 .replace 替换当前页面 ......
-                // MaterialPageRoute 继承自PageRoute，表示占有整个屏幕空间的路由页面
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return NewRoute();
-                }));
-              }
-            ),
-          ]
-        ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text('一共点击的次数：'),
+              new Text('$_counter',
+                  style: Theme.of(context).textTheme.headline4),
+              // 添加一个新的按钮，点击可以跳转去新的页面
+              FlatButton(
+                  child: Text('打开新页面'),
+                  textColor: Colors.blue,
+                  // 按钮点击事件
+                  onPressed: () {
+                    // Navigator 是一个路由管理的组件，提供了打开和退出路由的方法，通过栈来保存路由历史
+                    //   .push 打开新页面  .pop 回到上一个页面 .replace 替换当前页面 ......
+                    // MaterialPageRoute 继承自PageRoute，表示占有整个屏幕空间的路由页面
+                    // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    //   return NewRoute();
+                    // }));
+
+                    // 通过路由命名的方式打开新的页面
+                    // Navigator.pushName(context, 'new_page');
+
+                    // 打开路由时可以传递参数
+                    Navigator.of(context).pushName('new_pahe', '', 'hi');
+                  }),
+            ]),
       ),
       floatingActionButton: new FloatingActionButton(
         onPressed: _incrementCounter,
@@ -114,15 +126,13 @@ class _MyHomePageState extends State<MyHomePage> {
 // 封装打开demo1 demo2 demo3三个新页面的按钮组件
 // 自己想一下，如何实现从上面传递参数
 class OpenDemoPage extends StatelessWidget {
-  OpenDemoPage({Key key, @required this.title}) : super(key:key);
-  
+  OpenDemoPage({Key key, @required this.title}) : super(key: key);
+
   final String title;
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text(text)
-    );
+    return RaisedButton(child: Text(text));
   }
 }
 
